083b49b91e1359c7ccf3db1ef6352f8932b4963e
UMB200 <tomodachi200@gmail.com>
2017-10-06 14:01:26 -0400

    uploading all files with solution

--- makePerson.js
+++ makePerson.js
@@ -1,10 +1,14 @@
 
 var Person = function(firstAndLast) {
     // Complete the method below and implement the others similarly
-    this.getFullName = function() {
-      return "";
-    };
-    return firstAndLast;
+    var name; 
+    this.getFullName = () => name.join(' ');
+    this.getFirstName = ()=> name[0];
+    this.getLastName = () => name[1];
+    this.setFullName = (fullName) => {name = fullName.split(' ');};
+    this.setFirstName = (firstName) => {name[0] = firstName;};
+    this.setLastName = (lastName) => {name[1] = lastName;};
+    this.setFullName(firstAndLast);
 };
 
 var bob = new Person('Bob Ross');
